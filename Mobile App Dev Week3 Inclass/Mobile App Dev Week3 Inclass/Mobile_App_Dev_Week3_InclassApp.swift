1de8a044c08c1ec1481974233960cddc89c46743
Coco Dai <md6132@nyu.edu>
2026-09-22 15:40:38 -0400

    update

---
+++ Mobile App Dev Week3 Inclass/Mobile App Dev Week3 Inclass/Mobile_App_Dev_Week3_InclassApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_App_Dev_Week3_InclassApp.swift
+//  Mobile App Dev Week3 Inclass
+//
+//  Created by Mingshan Dai on 9/17/26.
+//
+
+import SwiftUI
+
+@main
+struct Mobile_App_Dev_Week3_InclassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Week3/Week3 Homework/Week3 Homework/ContentView.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+//struct ContentView: View {
+//    var body: some View {
+//        TabView {
+//            ArtView()
+//                .tabItem { Text("Art") }
+//
+//            Text("Random Emoji Art")
+//                .tabItem { Text("About") }
+//        }
+//    }
+//}
+
+struct ContentView: View {
+    let emojis = ["🌸", "🌷", "🌹", "🍀"]
+    @State var picture = [String]()
+
+    var body: some View {
+        VStack {
+            LazyVGrid(columns: [GridItem(), GridItem(), GridItem(), GridItem()]) {
+                ForEach(0..<picture.count, id: \.self) { number in
+                    Text(picture[number])
+                        .font(.largeTitle)
+                }
+            }
+            Button("New Garden") {
+                picture = (0..<16).map { _ in emojis.randomElement()! }
+            }
+        }
+        .padding()
+        .onAppear {
+            picture = (0..<16).map { _ in emojis.randomElement()! }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
